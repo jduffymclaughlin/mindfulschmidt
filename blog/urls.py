67954636f2faf1165b2c5cf6ffdc405f96ccbbe4
john-mclaughlin-fw <john.mclaughlin@flywire.com>
2018-06-02 13:59:43 -0400

    with db

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
+app_name = "blog"
+
 urlpatterns = [
-	path('', views.index, name='index')
+	path('', views.index, name='index'),
+	re_path(r'view/(?P<slug>[^\.]+)',
+    views.view_post, name='view_post')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse
+from .models import Blog
 
 
 def index(request):
-	return HttpResponse('hello blog world')
-	
+	posts = list(Blog.objects.all())
+	posts.reverse()
+
+	blog_posts = posts[:5]
+
+	return render_to_response('blog/index.html', {'posts': blog_posts,
+													'links': posts,})
+
+def view_post(request, slug):
+	blog_links = list(Blog.objects.all())
+	blog_links.reverse()
+
+	return render_to_response('blog/view_post.html', {
+		'post': get_object_or_404(Blog, slug=slug),
+		'links': blog_links,
+	})
